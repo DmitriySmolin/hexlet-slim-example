f239a8a3743f239ab35b4b97e18e260741040233
neox56 <neox56@gmail.com>
2023-10-15 12:53:04 +0400

    add flash for create user

--- public/index.php
+++ public/index.php
@@ -5,13 +5,20 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 use DI\Container;
+use Slim\Flash\Messages;
+use Slim\Views\PhpRenderer;
 
 $app = AppFactory::create();
+
+session_start();
 
 $container = new Container();
 $container->set('renderer', function () {
     // Параметром передается базовая директория, в которой будут храниться шаблоны
-    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+    return new PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function () {
+    return new Messages();
 });
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
@@ -54,8 +61,8 @@ $app->get('/users', function ($request, $response) use ($users) {
 
     $term = $request->getQueryParam('term');
     $filteredUsers = array_filter($users, fn($user) => str_contains($user, $term));
-    $params = ['users' => $filteredUsers];
-
+    $messages = $this->get('flash')->getMessages();
+    $params = ['users' => $filteredUsers, 'flash' => $messages];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 })->setName('users');
 
@@ -66,10 +73,16 @@ $app->get('/users/new', function ($request, $response, $args)  {
 
 $app->post('/users', function ($request, $response)  {
     $user = $request->getParsedBodyParam('user');
-    $user['id'] = uniqid();
-    $jsonData = json_encode($user);
-    file_put_contents('data/user.json', $jsonData);
+    $data = json_decode(file_get_contents('data/users.json'), true);
 
+    $user = [
+        "nickname" => $user['nickname'],
+        "email" => $user['email'],
+        "id" => uniqid()
+    ];
+    $data['users'][] = $user;
+    file_put_contents('data/users.json', json_encode($data, JSON_PRETTY_PRINT));
+    $this->get('flash')->addMessage('success', 'User was added successfully');
     return $response->withRedirect('/users');
 })->setName('users_create');
 
@@ -77,10 +90,10 @@ $app->get('/users/{id}', function ($request, $response, $args) {
     $id = $args['id'];
 
     // Если файл не найден или не существует пользователь с заданным id, возвращаем 404 ошибку
-    if (!file_exists('data/user.json')) {
+    if (!file_exists('data/users.json')) {
         return $response->withStatus(404);
     } else {
-        $data = file_get_contents('data/user.json');
+        $data = file_get_contents('data/users.json');
         $users = json_decode($data, true);
 
         foreach ($users['users'] as $user) {
@@ -92,5 +105,23 @@ $app->get('/users/{id}', function ($request, $response, $args) {
     }
     return $response->withStatus(404);
 })->setName('users_show');
+
+$app->get('/foo', function ($req, $res) {
+    // Добавление флеш-сообщения. Оно станет доступным на следующий HTTP-запрос.
+    // 'success' — тип флеш-сообщения. Используется при выводе для форматирования.
+    // Например, можно ввести тип success и отражать его зеленым цветом (на Хекслете такого много)
+    $this->get('flash')->addMessage('success', 'This is a message');
+
+    return $res->withRedirect('/bar');
+});
+
+$app->get('/bar', function ($req, $res) {
+    // Извлечение flash-сообщений, установленных на предыдущем запросе
+    $messages = $this->get('flash')->getMessages();
+    print_r($messages); // => ['success' => ['This is a message']]
+
+    $params = ['flash' => $messages];
+    return $this->get('renderer')->render($res, 'bar.phtml', $params);
+});
 
 $app->run();
